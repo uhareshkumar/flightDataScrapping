65fb40ba05fd0cbfa187649ddc8963a3dd2128cf
uhareshkumar <uharesh.kumar2015@vit.ac.in>
2020-05-14 22:25:00 +0530

    CORS error fixed

--- src/api/Scrapper.js
+++ src/api/Scrapper.js
@@ -2,5 +2,5 @@ import axios from 'axios';
 
 export default axios.create({
     baseURL: 'https://8b901e3b.ngrok.io',
-    headers: {'Content-Type': 'application/json'}
+    headers: {'Content-Type': undefined}
 });
